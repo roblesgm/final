335b705ec8b00330c5b34fb0562ba9c0c8f357cb
Grayson Robles <grayson04runner@gmail.com>
2019-12-17 07:23:01 -0700

    almost done

--- final.js
+++ final.js
@@ -1,42 +1,260 @@
-// Things I need to do to make this thing Work
-// - Use the formula on the Daily nutrients and calories website
-// - Adjust the question on the form to fit the data needed for the formula
-// - look at macronutrients website to get ideas for various food groups
-// - Ask charlie about the next page thing
-// - Use css to make the website actually look good
-
-if () {
-    sex == male
-    level == "in the gym most days";
-    66.47 + ( 13.75 * weight) + ( 5.003 * height) - ( 6.755 * age) == RMR;
-    (RMR * .4) + RMR == totalcalories;
-}
-// else () {
-//     655.1 + ( 9.563 * weight) + ( 1.850 * height) - ( 4.676 * age) == calories 
-// }
-
+let form = document.forms.nutrition;
+let sex = form.sex.value;
+let level = form.level.value;
 go.onclick = function() {
-    
+    let level = form.level.value;
+    if (sex == "male" && level == "little") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        console.log(weight)
+        console.log(height)
+        console.log(age)
+        let BMR = (6.23 * weight) + (12.7 * height) - (6.8 * age) + 66;
+        console.log(BMR)
+        let totalcalories = (BMR * 1.2);
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
+    if (sex == "male" && level == "lightly") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        // console.log(weight)
+        // console.log(height)
+        // console.log(age)
+        let BMR = (6.23 * weight) + (12.7 * height) - (6.8 * age) + 66;
+        // console.log(BMR)
+        let totalcalories = (BMR * 1.375);
+        console.log(totalcalories)
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
+    if (sex == "male" && level == "moderately") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        console.log(weight)
+        console.log(height)
+        console.log(age)
+        let BMR = (6.23 * weight) + (12.7 * height) - (6.8 * age) + 66;
+        console.log(BMR)
+        let totalcalories = (BMR * 1.55);
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
+    if (sex == "male" && level == "very_active") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        console.log(weight)
+        console.log(height)
+        console.log(age)
+        let BMR = (6.23 * weight) + (12.7 * height) - (6.8 * age) + 66;
+        console.log(BMR)
+        let totalcalories = (BMR * 1.725);
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
+    if (sex == "male" && level == "extra_active") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        console.log(weight)
+        console.log(height)
+        console.log(age)
+        let BMR = (6.23 * weight) + (12.7 * height) - (6.8 * age) + 66;
+        console.log(BMR)
+        let totalcalories = (BMR * 1.9);
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
+    if (sex == "female" && level == "little") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        console.log(weight)
+        console.log(height)
+        console.log(age)
+        let BMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
+        console.log(BMR)
+        let totalcalories = (BMR * 1.2);
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
+    if (sex == "female" && level == "lightly") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        console.log(weight)
+        console.log(height)
+        console.log(age)
+        let BMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
+        console.log(BMR)
+        let totalcalories = (BMR * 1.375);
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
+    if (sex == "female" && level == "moderately") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        console.log(weight)
+        console.log(height)
+        console.log(age)
+        let BMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
+        console.log(BMR)
+        let totalcalories = (BMR * 1.55);
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
+    if (sex == "female" && level == "very_active") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        console.log(weight)
+        console.log(height)
+        console.log(age)
+        let BMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
+        console.log(BMR)
+        let totalcalories = (BMR * 1.725);
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
+    if (sex == "female" && level == "extra_active") {
+        let weight = form.weight.value;
+        let height = form.height.value;
+        let age = form.age.value;
+        console.log("running")
+        console.log(weight)
+        console.log(height)
+        console.log(age)
+        let BMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
+        console.log(BMR)
+        let totalcalories = (BMR * 1.9);
+        document.getElementById("totalcalories").textContent = totalcalories;
+        let protien = (totalcalories * .2);
+        document.getElementById("protien").textContent = protien;
+        let carbs = (totalcalories * .5)
+        document.getElementById("carbs").textContent = carbs;
+        let fats = (totalcalories * .3)
+        document.getElementById("fats").textContent = fats;
+        let gramsp = (protien / 7.72)
+        document.getElementById("gramsp").textContent = gramsp;
+        let gramsc = (carbs / 7.72)
+        document.getElementById("gramsc").textContent = gramsc;
+        let gramsf = (fats / 7.72)
+        document.getElementById("gramsf").textContent = gramsf;
+    }
 }
 
-function openPage(pageName, elemt, color) {
-    let i, tabcontent, tablinks;
-    console.log (pageName);
-    tabcontent = document.getElementsByClassName("tabcontent")
-    for (i = 0; i < tablinks.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink")
-    for (i = 0; i < tablinks.length; i++) {
-        tabcontent[i].style.backgroundColor = "";
-    }
-    document.getElementById(pageName).style.display = "block";
-    elemt.style.backgroundColor = color;
-}
-document.getElementById("defaultOpen").click();
-(function () {
-    new FORM({
-        id: "form"
-    })
-})();
-document.addEventListener("DOMContentLoaded", ready);
+
+// document.addEventListener("DOMContentLoaded", ready);
